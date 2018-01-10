c9d9f598868cf449e24df9db9157f0af4464cc90
Colin Stevens <colin@colinjstevens.com>
2018-01-09 22:23:43 -0500

    Minor tweaks, most significantly moved from tabs to spaces

--- log/forms.py
+++ log/forms.py
@@ -1,40 +1,38 @@
 from django import forms
-from django.forms import extras
 from django.utils.safestring import mark_safe
 from django.utils import timezone
-from datetime import datetime
+import datetime
 
 class weblog_userForm(forms.Form):
-	nickname = forms.CharField(label='', initial="Nickname", widget=forms.TextInput(attrs={
-		'required' : False,
-		'id' : False,
-		'onfocus' : mark_safe("if(this.value=='Nickname'){this.value='';}"),
-		'onblur' : mark_safe("if(!this.value){this.value='Nickname';}"),
-	}))
-	password = forms.CharField(label='', initial="Password", widget=forms.PasswordInput(render_value=True, attrs={
-		'required' : False,
-		'id' : False,
-		'onfocus' : mark_safe("if(this.value=='Password'){this.value='';}"),
-		'onblur' : mark_safe("if(!this.value){this.value='Password';}"),
-	}))
-	message = forms.CharField(label='', initial="Message..", widget=forms.TextInput(attrs={
-		'required' : False,
-		'id' : False,
-		'onfocus' : mark_safe("if(this.value=='Message..'){this.value='';}"),
-		'onblur' : mark_safe("if(!this.value){this.value='Message..';}"),
-	}))
+    nickname = forms.CharField(label='', initial="Nickname", widget=forms.TextInput(attrs={
+        'required' : False,
+        'id' : False,
+        'onfocus' : mark_safe("if(this.value=='Nickname'){this.value='';}"),
+        'onblur' : mark_safe("if(!this.value){this.value='Nickname';}"),
+    }))
+    password = forms.CharField(label='', initial="Password", widget=forms.PasswordInput(render_value=True, attrs={
+        'required' : False,
+        'id' : False,
+        'onfocus' : mark_safe("if(this.value=='Password'){this.value='';}"),
+        'onblur' : mark_safe("if(!this.value){this.value='Password';}"),
+    }))
+    message = forms.CharField(label='', initial="Message...", widget=forms.TextInput(attrs={
+        'required' : False,
+        'id' : False,
+        'onfocus' : mark_safe("if(this.value=='Message...'){this.value='';}"),
+        'onblur' : mark_safe("if(!this.value){this.value='Message...';}"),
+    }))
 
 class weblog_dlForm(forms.Form):
-	date = forms.DateField(
-		widget=extras.SelectDateWidget(
-			years=(list(reversed(range(2015,datetime.now().year+1)))),
-		),
-		initial=timezone.now(),
-	)
-	format_choices = (
-		('html', 'HTML'),
-		('json', 'JSON'),
-		('xml', 'XML'),
-		('yaml', 'YAML'),
-	)
-	log_format = forms.ChoiceField(choices=format_choices)
+    date = forms.DateField(widget=forms.SelectDateWidget(
+        years=(list(reversed(range(2015,datetime.datetime.now().year+1)))),
+        ),
+        initial=datetime.date.today,
+    )
+    format_choices = (
+        ('html', 'HTML'),
+        ('json', 'JSON'),
+        ('xml', 'XML'),
+        ('yaml', 'YAML'),
+    )
+    log_format = forms.ChoiceField(choices=format_choices)
--- log/models.py
+++ log/models.py
@@ -1,40 +1,35 @@
 from django.db import models
 
 class Line(models.Model):
-	message = models.TextField() 
-	nick = models.CharField(max_length=50)
-	hostname = models.CharField(max_length=300)
-	channel = models.CharField(max_length=200)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	PRIVMSG = 'PMSG'
-	NOTICE = 'NTCE'
-	ACTION = 'ACTN'
-	JOIN = 'JOIN'
-	PART = 'PART'
-	QUIT = 'QUIT'
-	NICK = 'NICK'
-	TOPIC = 'TPIC'
-	msgType_choices = (
-		(PRIVMSG, 'PRIVMSG'),
-		(NOTICE, 'NOTICE'),
-		(ACTION, 'ACTION'),
-		(JOIN, 'JOIN'),
-		(PART, 'PART'),
-		(QUIT, 'QUIT'),
-		(NICK, 'NICK'),
-		(TOPIC, 'TOPIC'),
-	)
-	msgType = models.CharField(max_length=4, choices=msgType_choices, default=PRIVMSG)
+    message = models.TextField(blank=True, default="") # Allow blank entries as not all lines will have a message (QUIT, JOIN, etc.) 
+    nick = models.CharField(max_length=50)
+    hostname = models.CharField(max_length=300)
+    channel = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    LINE_TYPES = (
+        ('PMSG', 'PRIVMSG'),
+        ('SMSG', 'SOCKETMSG'),
+        ('WMSG', 'WEBMSG'),
+        ('NTCE', 'NOTICE'),
+        ('ACTN', 'ACTION'),
+        ('JOIN', 'JOIN'),
+        ('PART', 'PART'),
+        ('QUIT', 'QUIT'),
+        ('NICK', 'NICK'),
+        ('TPIC', 'TOPIC'),
+    )
+    msgType = models.CharField(max_length=4, choices=LINE_TYPES, default='PRIVMSG')
 
-	def __str__(self):
-		return self.message
+    def __str__(self):
+        return "(%s / %s) %s - %s" % (self.timestamp, self.channel, self.nick, self.message)
 
 class webLine(models.Model):
-	user = models.CharField(max_length=50)
-	ipAddress = models.GenericIPAddressField()
-	timestamp = models.DateTimeField(auto_now_add=True)
-	message = models.TextField()
+    user = models.CharField(max_length=50)
+    channel = models.CharField(max_length=200)
+    ipAddress = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
 
 class banned_ips(models.Model):
-	bannedIp = models.GenericIPAddressField()
+    bannedIp = models.GenericIPAddressField()
 
