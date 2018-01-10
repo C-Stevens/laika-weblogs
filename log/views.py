6eb4a43300e639dd7a9a381e3fc239568cc2c713
Colin Stevens <colin@colinjstevens.com>
2018-01-09 22:25:01 -0500

    Complete rewrite

--- log/views.py
+++ log/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from django.views.decorators.http import require_GET
 from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.contrib import messages
@@ -18,117 +19,134 @@ import time
 import json
 
 SOCKET_ADDR = 'localhost'
-SOCKET_PORT = 9989
+SOCKET_PORT = 9977
 
 def root(request):
-	return HttpResponseRedirect('/weblog') # Default starting URL
+    return HttpResponseRedirect('/log') # Default starting URL
 
-def weblogs(request):
-	return HttpResponseRedirect('/weblog/pwiki') # Default channel
+def weblogs(request, channel): # Legacy redirect
+    return HttpResponseRedirect('/log/%s' % channel)
+
+def log(request):
+    return HttpResponseRedirect('/log/pwiki') # Default channel
+
+@require_GET
+def api(request, channel, latest_id):
+    latest_id = int(latest_id)
+    requestedLines = requestedLines = Line.objects.filter(channel=channel).order_by('-id')
+    if latest_id:
+        requestedLines = requestedLines.filter(id__gt=latest_id)
+
+    # Django does not allow for negative indexing while filtering queries, so they are sorted ascending in the query [order_by(-id)] and limited to 100 items
+    # However this produces a list with newest entry first, which is not useful for a chronological log. Once the set is obtained from the query, reverse it for the view.
+    json_data = serializers.serialize('json', list(requestedLines[:100])[::-1])
+    return HttpResponse(json_data, content_type="application/json")
 
 def channel(request, channel):
-	# Disabed for suspected performance issues:
-	#if not Line.objects.filter(channel=channel): # If there's no lines, don't bother rendering a log_page
-		#return render(request, 'log/err.html', RequestContext(request, {'errName': "No log data", 'errDetails': "No IRC lines could be found for this channel.",}))
-	if request.method == 'POST':
-		user_form = weblog_userForm(request.POST)
-		if user_form.is_valid():
-			json_response = {}
-			for i in banned_ips.objects.order_by('bannedIp'): # User is banned from posting messages with the web form
-				if get_ip(request) == i.bannedIp:
-					json_response['user_can_post'] = False
-					return HttpResponse(json.dumps(json_response), content_type="application/json")
-			json_response['user_can_post'] = True
-			json_response['valid_nickname'] = user_form.cleaned_data['nickname'] not in ('Nickname', '',)
-			json_response['valid_message'] = user_form.cleaned_data['message'] not in ('Message..', '',)
-			l = webLine(ipAddress=get_ip(request), user=user_form.cleaned_data['nickname'], message=user_form.cleaned_data['message'])
-			l.save()
-			if not json_response['valid_nickname'] or not json_response['valid_message']:
-				json_response['valid_password'] = None
-				return HttpResponse(json.dumps(json_response), content_type="application/json")
+    # Disabed for suspected performance issues:
+    #if not Line.objects.filter(channel=channel): # If there's no lines, don't bother rendering a log_page
+        #return render(request, 'log/err.html', RequestContext(request, {'errName': "No log data", 'errDetails': "No IRC lines could be found for this channel.",}))
+    user_form = weblog_userForm(request.POST or None)
+    if request.method == 'POST' and user_form.is_valid():
+        nickname = user_form.cleaned_data['nickname']
+        message = user_form.cleaned_data['message']
+        password = user_form.cleaned_data['password']
+        ip = get_ip(request)
+        timestamp = datetime.datetime.now()
+        postDetails = { 'user_not_banned'           : True,
+                        'nickname_not_default'  : nickname not in ["Nickname", ""],
+                        'message_not_default'   : message not in ["Message...", ""],
+                        'backend_alive'     : None, # connect
+                        'valid_password'    : None, # auth
+                        'send_success'      : None, # send_line
+                        
+            }
+        # Check to see if this IP is banned from posting messages. If it is, mark the error and return
+        for i in banned_ips.objects.order_by('bannedIp'):
+            if ip == i.bannedIp:
+                postDetails['user_not_banned'] = False
+                return HttpResponse(json.dumps(postDetails), content_type="application/json")
+        # Check to make sure a default (empty) form was submitted. If it was, do not bother processing and return
+        if not postDetails['nickname_not_default'] or not postDetails['message_not_default']:
+            return HttpResponse(json.dumps(postDetails), content_type="application/json")
 
-			weblog = webloglib.weblog_manager(SOCKET_ADDR, SOCKET_PORT)
-			weblog.auth(user_form.cleaned_data['password'].encode('utf-8'))
-			json_response['valid_password'] = weblog.send_line(channel, user_form.cleaned_data['nickname'], user_form.cleaned_data['message'])
-			return HttpResponse(json.dumps(json_response), content_type="application/json")
-		else: # Generate a JSON response to draw a useful error message
-			json_response = {}
-			for key in user_form.errors.as_data():
-				json_response['valid_'+key] = False
-			return HttpResponse(json.dumps(json_response), content_type="application/json")
-	else:
-		user_form = weblog_userForm()
-	if (request.is_ajax()):
-		latest_line_id = request.GET.get('latest_id', '')
-		if latest_line_id == '-1': # Return all lines
-			json_data = serializers.serialize("json", list(reversed(Line.objects.filter(channel=channel).order_by('-id')[:100])))
-		else:
-			json_data = serializers.serialize("json", list(reversed(Line.objects.filter(id__gt=latest_line_id, channel=channel).order_by('-id')[:100])))
-		return HttpResponse(json_data, content_type="application/json")
+        client = webloglib.weblog_client(SOCKET_ADDR, SOCKET_PORT)
+        try:
+            client.connect()
+        except (ConnectionError, TimeoutError, OSError):
+            postDetails['backend_alive'] = False
+            return HttpResponse(json.dumps(postDetails), content_type="application/json")
+        try:
+            client.auth(password)
+        except webloglib.invalidProofError:
+            postDetails['valid_password'] = False
+            return HttpResponse(json.dumps(postDetails), content_type="application/json")
+        postDetails['valid_password'] = True
+        
+        lineData = {'nickname'  : nickname,
+                    'timestamp' : timestamp,
+                    'message'   : message,
+                    'channel'   : channel }
+        try:
+            client.send_line(lineData)
+        except Exception:
+            postDetails['send_success'] = False
+            return HttpResponse(json.dumps(postDetails), content_type="application/json")
+        # Log this successfully sent line to the special database for weblog lines
+        webLine(user=nickname, ipAddress=ip, timestamp=timestamp, message=message, channel=channel).save()
 
-	context_dict = {
-		'channel' : channel,
-		'user_form' : user_form,
-		'dl_form' : weblog_dlForm(),
-	}
-	context = RequestContext(request, context_dict)
-	return render(request, 'log/log_page.html', context)
+        return HttpResponse(json.dumps(postDetails), content_type="application/json")
+    if request.method == "GET":
+        context_dict = {'channel' : channel,
+                        'user_form' : weblog_userForm(),
+                        'dl_form' : weblog_dlForm(),
+        }
+        return render(request, 'log/log_page.html', context_dict)
 
 def download(request, channel, **kwargs):
-	if request.method == "POST":
-		dl_form = weblog_dlForm(request.POST)
-		if dl_form.is_valid():
-			formDate = dl_form.cleaned_data['date']
-			date_requested = {
-				'year' : formDate.year,
-				'month' : formDate.month,
-				'day' : formDate.day,
-			}
-			dl_format = dl_form.cleaned_data['log_format']
-		else:
-			return render(request, 'log/err.html', RequestContext(request, {'errName': "Invalid form data", 'errDetails': "Form data failed to be validated (contains one or more errors).",}))
-	elif request.method == "GET":
-		if not kwargs.get('date'): # A request to just .../dl/
-			return HttpResponseRedirect("/weblog/{0}".format(channel))
+    date = kwargs.get('date')
+    format = kwargs.get('format')
+    date_requested = {
+        'year' : 0,
+        'month' : 0,
+        'day'   : 0,
+    }
 
-		splitDate = kwargs.get('date').split('-') # Date format: YYYY-MM-DD
-		date_requested = {
-			'year' : int(splitDate[0]),
-			'month' : int(splitDate[1]),
-			'day' : int(splitDate[2]),
-		}
-		dl_format = kwargs.get('format')
-	startDate = datetime.date(date_requested['year'], date_requested['month'], date_requested['day'])
-	try:
-		endDate = datetime.date(date_requested['year'], date_requested['month'], date_requested['day']+1)
-	except ValueError: # Day requested is at the end of the month. Bump month or roll it over to January
-		nextMonth = date_requested['month']+1 if date_requested['month'] < 12 else 1
-		endDate = datetime.date(date_requested['year'], nextMonth, 1)
-
-	logLines = Line.objects.filter(channel=channel, timestamp__range=(startDate, endDate))
-	if not logLines:
-			return render(request, 'log/err.html', RequestContext(request, {'errName': "No lines returned", 'errDetails': "Failed to fetch any IRC lines for this channel.",}))
-	if dl_format == 'xml':
-		line_data = xml_log.createLog(logLines)
-		formatType = "application/xml"
-	elif dl_format == 'json':
-		line_data = serializers.serialize("json", logLines)
-		formatType = "application/json"
-	elif dl_format == 'yaml':
-		line_data = serializers.serialize("yaml", logLines)
-		formatType = "text/x-yaml"
-	elif dl_format == 'html':
-		formatType = "text/html"
-
-	if request.method == "POST": # Redirect request, making it a GET
-		return HttpResponseRedirect("/weblog/{0}/dl/{1:0>4}-{2:0>2}-{3:0>2}.{4}".format(channel, date_requested['year'], date_requested['month'], date_requested['day'], dl_format))
-	elif request.method == "GET": # Serve the log
-		if formatType == 'text/html': # Special case requiring a template render
-			context_dict = {
-				'channel' : channel,
-				'lines' : logLines,
-			}
-			context = RequestContext(request, context_dict)
-			return render(request, 'log/log_dl.html', context)
-		else:
-			return HttpResponse(line_data, formatType)
+    if request.method == "POST":
+        # Extract request data from POST form, then redirect the request to be a GET
+        f = weblog_dlForm(request.POST)
+        if f.is_valid():
+            date_requested['year'] = f.cleaned_data['date'].year
+            date_requested['month'] = f.cleaned_data['date'].month
+            date_requested['day'] = f.cleaned_data['date'].day
+        format = f.cleaned_data['log_format']
+        return HttpResponseRedirect("%d-%02d-%02d.%s" % (date_requested.get('year'), date_requested.get('month'), date_requested.get('day'), format))
+    splitDate = date.split('-')
+    try:
+        date_requested['year'] = int(splitDate[0])
+        date_requested['month'] = int(splitDate[1])
+        date_requested['day'] = int(splitDate[2])
+    except ValueError:
+        return render(request, 'log/err.html', RequestContext(request, {'errName': "No lines returned", 'errDetails': "Failed to fetch any IRC lines for this channel.",}))
+    
+    startDate = datetime.date(date_requested['year'], date_requested['month'], date_requested['day'])
+    try:
+        endDate = datetime.date(date_requested['year'], date_requested['month'], date_requested['day']+1)
+    except ValueError: # Day requested is at the end of the month. Bump to the first of the next month
+        nextMonth = date_requested['month']+1 if date_requested['month'] < 12 else 1
+        endDate = datetime.date(date_requested['year'], nextMonth, 1)
+    logLines = Line.objects.filter(channel=channel, timestamp__range=(startDate, endDate))
+    
+    if format == 'xml':
+        line_data = xml_log.createLog(logLines)
+        formatType = "application/xml"
+    elif format == 'json':
+        line_data = serializers.serialize("json", logLines)
+        formatType = "application/json"
+    elif format == 'yaml':
+        line_data = serializers.serialize("yaml", logLines)
+        formatType = "text/x-yaml"
+    elif format == 'html':
+        line_data = serializers.serialize('json', logLines)
+        return render(request, 'log/log_dl.html', {'channel': channel, 'lines' : line_data,})
+    return HttpResponse(line_data, formatType)
--- log/webloglib.py
+++ log/webloglib.py
@@ -3,35 +3,63 @@ import time
 import os
 import struct
 import hashlib
+import json
+import datetime
 
-class weblog_manager(object):
-	def __init__(self, host, port):
-		self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		try:
-			self.conn.connect((host, port))
-		except:
-			pass #TODO: Return back statement saying laika is unavailable
-	def receive(self, bytes_requested):
-		data = b''
-		while len(data) != bytes_requested:
-			data += self.conn.recv(bytes_requested - len(data))
-		return data
-	def auth(self, password):
-		self.conn.send(b"WOOF") # Initiate auth sequence
-		client_time = int(time.time())
-		client_salt = os.urandom(10)
-		self.conn.send(struct.pack('!L10s', client_time, client_salt))
+class weblogError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+class timeMisMatchError(weblogError):
+    def __init__(self, clientTime):
+        Exception.__init__(self, "Server rejected client time (%d) [TIME_MISMATCH]" % clientTime)
+class invalidProofError(weblogError):
+    def __init__(self):
+        Exception.__init__(self, "Server rejected the client proof [INVALID_PROOF]")
 
-		server_time, server_salt = struct.unpack('!L10s', self.receive(struct.calcsize('!L10s')))
-		proof = hashlib.sha224()
-		proof.update(struct.pack('!L', server_time) + struct.pack('!L', client_time) + server_salt + client_salt + password)
-		self.conn.send(proof.digest())
-	def send_line(self, channel, nickname, message):
-		socket_line = ("PRIVMSG %s :[%s] %s\r\n"%('#'+channel, nickname, message)).encode('utf-8')
-		try:
-			self.conn.send(struct.pack('!I', len(socket_line)))
-			self.conn.send(socket_line)
-		except Exception as e:
-			print("Weblog Manager send error %d: %s"%(e.args[0], e.args[1]))
-			return False
-		return True
+def datetimeSerialize(timestamp):
+    if isinstance(timestamp, (datetime.datetime, datetime.date)):
+        return timestamp.isoformat()
+    else:
+        raise TypeError("Expected type datetime but received type %s" % repr(type(repr(timestamp))))
+
+class weblog_client(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def connect(self):
+        self.socket.connect((self.host, self.port))
+    def receive(self, bytes_requested):
+        data = b''
+        while len(data) != bytes_requested:
+            data += self.socket.recv(bytes_requested - len(data))
+        return data
+    def auth(self, password):
+        self.socket.send(b"WOOF") # Initiate auth sequence
+        ## t0, s0
+        client_time = int(time.time())
+        client_salt = os.urandom(10)
+        self.socket.send(struct.pack('!L10s', client_time, client_salt))
+        
+        ## r0
+        response = self.receive(struct.calcsize('19s')).rstrip(b'\x00') # Strip NULL chars in case of return message < 19 chars
+        if response != b"laika:OK":
+            if response == b'laika:TIME_MISMATCH':
+                raise timeMisMatchError(client_time)
+        
+        ## t1, s1 / t0+t1+s0+s1
+        server_time, server_salt = struct.unpack('!L10s', self.receive(struct.calcsize('!L10s')))
+        proof = hashlib.sha224()
+        proof.update(struct.pack('!L', server_time) + struct.pack('!L', client_time) + server_salt + client_salt + bytes(password, 'utf8'))
+        self.socket.send(proof.digest())
+        
+        ## r1
+        response = self.receive(struct.calcsize('19s')).rstrip(b'\x00') # Strip NULL chars in case of return message < 19 chars
+        if response != b'laika:OK':
+            if response == b'laika:INVALID_PROOF':
+                raise invalidProofError
+    def send_line(self, data):
+        serializedData = json.dumps(data, default=datetimeSerialize).encode('utf8')
+        self.socket.send(struct.pack('!I', len(serializedData)))
+        self.socket.send(serializedData)
+        self.socket.close()
